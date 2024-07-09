76cd90badd04b896ffd1f09c6ad8238b0250060a
Евгений Волков <evgenijvolkov@MacBook-Pro-16.local>
2024-07-09 20:41:06 +0300

    HW-3 GIT

---
+++ HW3 Evgeniy Nesterenko/HW-3 Evgeniy Nesterenko/HW-3 Evgeniy Nesterenko/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  HW-3 Evgeniy Nesterenko
+//
+//  Created by Евгений Волков on 13.06.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+// Изменения №1 для создания 1-го commit
+var Number1: Int = 5
+
+// Изменения №2 для создания 2-го commit
+var Brand: String = "Apple"
+
+// Изменения №3 для создания 3-го commit
+var Brand1: String = "Apple Inc"
+
+// Проект был полностью удален и заново скачен из GitHub
+// Изменения №4 для создания 4-го commit
+var Brand2: String = "Apple Inc."
+
+// Изменения №5 для создания 5-го commit
+var Brand3: String = "Apple Inc.!"
+
+// Была создана новая ветка - HomeWork3 и сделан commit на этой ветке
+// Изменения №6 для создания 6-го commit
+var Brand4: String = "Apple Inc.!!!"
